e536449087593e9aa669c709f2988d4b1a76edd8
SeraphimSage <kenneth.pinkerton90@gmail.com>
2020-01-11 10:34:00 -0700

    Finishes initial Oregon Trail assessment.

--- script.js
+++ script.js
@@ -17,39 +17,59 @@ Traveler.prototype = {
     },
     eat: function () {
         // Consumes 1 unit of the traveler's food. If the traveler doesn't have any food to eat, the traveler is no longer healthy.
-        this.food -= 1;
-        if(this.food <= 0){
+        if(this.food === 0){
             this.isHealthy = false
+        }
+        else{
+            this.food -= 1;
         }
     }
 }
 
 
 
-function Wagon (capacity=3, passengers) {
+function Wagon (capacity, passengers) {
     //a capacity (number) that must be provided as a parameter to the constructor, sets the maximum number of passengers the wagon can hold
     this.capacity = capacity;
 
     //a passengers list (array) which is initially empty
-    this.passengers = passengers;
+    this.passengers = [];
 }
 Wagon.prototype = {
     constructor: Wagon,
     getAvailableSeatCount: function () {
         // Return the number of empty seats, determined by the capacity set when the wagon was created, compared to the number of passengers currently on board.
-        this.getAvailableSeatCount = this.capacity - this.passengers;
+        return this.capacity - this.passengers.length;
     },
-    join: function () {
+    join: function (traveler) {
         // Add the traveler to the wagon if there is space. If the wagon is already at maximum capacity, don't add them
-        if(this.getAvailableSeatCount >= 1){
-            this.Wagon.push(this.passengers) = this.Traveler
+        if(this.getAvailableSeatCount() >= 1){
+            this.passengers.push(traveler);
+        }
+
+        if(this.getAvailableSeatCount() === 0){
+            return "Sorry this wagon is full."
         }
     },
     shouldQuarantine: function() {
         //Return true if there is at least one unhealthy person in the wagon. Return false if not.
+        let checkup = "";
+        for (let i = 0; i <= 1; i++) {
+            if (this.passengers[i].isHealthy === false) {
+                checkup = true
+            }
+            else {
+                checkup = false
+            }
+        }
+        return checkup
     },
     totalFood: function() {
         //Return the total amount of food among all occupants of the wagon.
+        for(let i = 0; i <= this.passengers.length; i++){
+            let sum = this.passengers[i].food
+            return sum;
+        }
     }
 }
 
